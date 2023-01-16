7210bdbd696b9996ad19a68f4e90c100f49a76a2
Agus <pymobile123@gmail.com>
2023-01-17 01:11:36 +0700

    remove unused files

--- routes/index.dart
+++ routes/index.dart
@@ -1,7 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:hive/hive.dart';
-import 'package:isar/isar.dart';
-import 'package:massanotif/objmodel.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   var box = Hive.box("jos");
