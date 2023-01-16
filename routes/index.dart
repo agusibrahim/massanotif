fe7504f9ca61f0b67fbf23d556f2e984a214ddd1
Agus <pymobile123@gmail.com>
2023-01-17 01:39:08 +0700

    edit index

--- routes/index.dart
+++ routes/index.dart
@@ -2,7 +2,5 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:hive/hive.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  var box = Hive.box("jos");
-  box.put("visit", box.get("visit") == null ? 1 : box.get("visit") + 1);
-  return Response(body: 'Welcome to Dart Frog! (${box.get("visit")})');
+  return Response(body: 'Welcome to massa signaling, visit https://github.com/agusibrahim/massanotif/ for more info');
 }
